847893e253625e9d614b30f381c76d26b41ee624
piyarattt <107599863+piyarattt@users.noreply.github.com>
2022-06-16 13:30:23 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1655360831,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/car-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/star-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/home-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
